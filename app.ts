5d4d5a261e0a7bc4943ac3dd04e9afbec79392db
Filip Chalupa <chalupa.filip@gmail.com>
2022-05-13 00:04:19 +0200

    Link lights

--- app.ts
+++ app.ts
@@ -2,19 +2,49 @@ import { delay } from './utils/delay.ts'
 
 console.log('Starting')
 
+const rgbLights = [
+	{
+		linked: false,
+		mac: 'EC:C7:10:29:B1:22',
+	},
+	{
+		linked: false,
+		mac: 'C2:BF:C7:30:AA:F3',
+	},
+]
+
+console.log('Discovering RGB lights')
 while (true) {
-	console.log('Discovering devices')
 	try {
 		const response = await fetch(
 			'http://localhost:8080/NeewerLite-Python/doAction?discover',
 		)
-		console.log(response.ok)
 		if (response.ok) {
+			const response = await fetch(
+				'http://localhost:8080/NeewerLite-Python/doAction?list',
+			)
 			const text = await response.text()
-			console.log(text)
+			for (const rgbLight of rgbLights) {
+				if (text.includes(rgbLight.mac)) {
+					rgbLight.linked = true
+				}
+			}
 		}
 	} catch (error) {
 		console.error(error)
 	}
+
+	const notLinkedRgbLights = rgbLights.filter((rgbLight) => !rgbLight.linked)
+
+	if (notLinkedRgbLights.length === 0) {
+		console.log('RGB lights are linked')
+		break
+	} else {
+		notLinkedRgbLights.forEach((rgbLight) => {
+			console.log(`RGB light ${rgbLight.mac} is not linked yet`)
+		})
+	}
+
 	await delay(1000)
+	console.log('Retrying')
 }
