7a9649bdf45eb5420479ad9c9079c83318c83366
Filip Chalupa <chalupa.filip@gmail.com>
2023-04-21 21:14:30 +0200

    Update

--- app.ts
+++ app.ts
@@ -1,14 +1,16 @@
 import { delay } from './utils/delay'
 
 console.log('Starting')
-const lightBaseUrl = 'http://10.10.241.189'
+const lightBaseUrl = 'http://192.168.1.254'
 
 while (true) {
 	try {
+		console.log('Looping')
 		const response = await fetch(
 			'https://chytra-domacnost.onrender.com/api/physical',
 		)
 		const data = await response.json()
+		console.log('Updating light')
 		await fetch(
 			`${lightBaseUrl}/cm?cmnd=Power%20${data.light === 'on' ? 'On' : 'Off'}`,
 		)
